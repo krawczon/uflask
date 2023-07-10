b6d96725863d6b6bba00aea2aff647b7dd9b075e
krawczod <damian.krawczonek@gmail.com>
2023-07-10 11:52:00 +0200

    rebuild

--- uflask.py
+++ uflask.py
@@ -2,62 +2,68 @@ try:
     import usocket as socket
 except ModuleNotFoundError:
     print('can not import usocket library')
-    print('This is propably becouse is not on micropython enviroment')
-    print('importting socket instead')
 finally:
     import socket
 import time
-
-# Dictionary to store route handlers
+from render_template import render_template
+from make_response import make_response
+from request import Request
+from get_method import get_method
+from parse_query_string import parse_query_string
+        
+request = Request()
 
 class uFlask():
-    def __init__(self):
-        self.route_handlers = {}
+    def __init__(self,__name__, request = request):
 
-    def route(self, path):
+        self.__name__ = __name__
+        self.route_handlers = {}
+        self.host = '0.0.0.0'
+        self.port = 5000
+        self.clients = []
+        self.request = request
+
+    def route(self, path, methods = ['GET']):
         # Decorator to register a route handler
         def decorator(func):
             self.route_handlers[path] = func
             return func
         return decorator
 
-    def run(self, hostname = '0.0.0.0', port = 5000):
+    def run(self, host = '0.0.0.0', port = 5000, debug = False):
+        self.host = host
+        self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((hostname, port))
+        self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
-
-        print('Running server '+hostname, 'on port ',str(port))
-
-        # Set the server socket to non-blocking mode
         self.server_socket.setblocking(False)
+        self.quit = False
+        self.run = True
+        print(host, port)
+        print(self.route_handlers)
 
-        clients = []
-        
-        run = True
-
-        while True:
+        while self.run:
             try:
                 # Accept new client connections
                 client_socket, client_address = self.server_socket.accept()
-                clients.append(client_socket)
-                print("New client connected:", client_address)
+                self.clients.append(client_socket)
             except OSError:
                 pass
 
             # Iterate over connected clients
-            for client_socket in clients:
+            for client_socket in self.clients:
                 try:
                     # Receive data from the client
                     request = client_socket.recv(1024)
-                    request = str(request)
+                    request = request.decode()
+                    self.request.method = get_method(request)
+                    self.request.form = parse_query_string(request)
                     print('request ------------------------------------')
                     print(request)
-                    print('clietns ------------------------------------')
-                    print(clients)
+                    print(self.request.method)
 
                     # Parse the request to extract the path
                     path = self.parse_request_path(request)
-
                     # Find the corresponding route handler
                     handler = self.route_handlers.get(path)
 
@@ -66,16 +72,32 @@ class uFlask():
                         response = handler()
                         client_socket.send(response.encode('utf-8'))
                     else:
-                        client_socket.send('HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\nnot ok'.encode('utf-8'))
+                        message = '''HTTP/1.1 404 Not Found\r\n
+                        Content-Type: text/html\r\n\r\n
+                        not ok'''.encode('utf-8')
+                        client_socket.send(message)
 
                     # Close the client connection
                     client_socket.close()
-                    clients.remove(client_socket)
+                    self.clients.remove(client_socket)
+
+                    if self.quit:
+                        self.terminate()
+
                 except OSError:
                     pass
 
             # Add a small delay to prevent high CPU usage
             time.sleep(0.01)
+
+    def terminate(self):
+        for client_socket in self.clients:
+            client_socket.close()
+            self.clients.remove(client_socket)
+
+        self.run = False
+        self.server_socket.close()
+        print('uflask terminated')
 
     def parse_request_path(self, request):
         # Parse the request to extract the path
@@ -86,15 +108,3 @@ class uFlask():
             if len(parts) >= 2:
                 return parts[1]
         return ''
-
-def make_response(*args, **kwargs):
-    response = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
-    return response + args[0]
-
-def render_template(*args, **kwargs):
-    file = open(args[0], "r")
-    content = file.read()
-    formated_content = content.format(**kwargs)
-    file.close()
-    return formated_content
-
